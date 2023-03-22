8ef1e2f295437f9e53ce2687e2166be3ceffc68e
Lukas Santing <lukas.santing@gmail.com>
2023-03-22 17:31:33 +0100

    A hobby Python project for the automatic generation of image avatars. Using OpenAi's ChatGPT & following tutorials (linked in each file).

---
+++ Avatar Generator/art_avatar_generator.py
@@ -0,0 +1,72 @@
+# ! DISCLAIMER !
+# Not my idea originally, based on an article by Real Python
+# Link to original article: https://realpython.com/generate-images-with-dalle-openai-api/
+
+# Imports
+import json
+import os
+import openai
+from base64 import b64decode
+from pathlib import Path
+import sys
+
+
+def generate_image_from_prompt():
+    '''
+    Function to take a prompt and request the openai dall-e to create an image with that prompt
+    '''
+
+    # input
+    input_prompt = input("Provide a sentence to generate an image from: ")
+    output_dir = Path.cwd() / "responses"
+    output_dir.mkdir(exist_ok=True)
+
+    # get api key (make sure to follow steps in "commands_to_run" to get this to work)
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+
+    # generate image
+    response = openai.Image.create(
+        prompt=input_prompt,
+        n=1,
+        size="256x256",
+        response_format="b64_json"
+    )
+
+    file_name = output_dir / f"{input_prompt[:5]}-{response['created']}.json"
+
+    with open(file_name, mode="w", encoding="utf-8") as file:
+        json.dump(response, file)
+
+    convert_data_to_image(file_name)
+
+
+def convert_data_to_image(file_name):
+    '''
+    Function to take a json file containing an encoded image & save the image as a .png
+    '''
+
+    # get files and paths
+    data_dir = Path.cwd() / "responses"
+    json_file = data_dir / file_name
+    image_dir = Path.cwd() / "images" / json_file.stem
+
+    image_dir.mkdir(parents=True, exist_ok=True)
+
+    # open file to convert
+    with open(json_file, mode="r", encoding="utf-8") as file:
+        response = json.load(file)
+
+    # decode image from encoded text and save to .png file
+    for index, image_dict in enumerate(response["data"]):
+        image_data = b64decode(image_dict["b64_json"])
+        image_file = image_dir / f"{json_file.stem}-{index}.png"
+        with open(image_file, mode="wb") as png:
+            png.write(image_data)
+
+
+def main(args):
+    generate_image_from_prompt()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
